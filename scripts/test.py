16f7a870db330a93654e03fa7a10a34c7ebacd35
changja88 <changja00@gmail.com>
2020-04-20 15:02:55 +0900

    save

--- scripts/test.py
+++ scripts/test.py
@@ -1,11 +1,28 @@
-from margin.models import Margin
+sale = 289000
+fee = 16588
+hitomi = 220000
 
 
 def run():
-    a = Margin.objects.all()
+    sale_tax1 = (sale / 11) * 10
+    sale_tax2 = (sale / 11)
 
-    total = 0
-    for abc in a:
-        total+=abc.margin
+    fee_tax1 = (fee / 11) * 10
+    fee_tax2 = (fee / 11)
 
+    hitomi_tax1 = (hitomi / 11) * 10
+    hitomi_tax2 = (hitomi / 11)
+
+    margin = round(sale - fee - hitomi)
+    tax1 = round(sale_tax1 - fee_tax1 - hitomi_tax1)
+    tax2 = round(sale_tax2 - fee_tax2 - hitomi_tax2)
+
+    print(margin)
+    print(tax1)
+    print(tax2)
+
+    total = margin - (tax1*0.06) - tax2
     print(total)
+
+
+run()
